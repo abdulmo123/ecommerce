c7c35bc846e2491522df47f9482495e951e13500
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2023-04-05 17:16:35 -0500

    setup order model

--- ecommerce-backend/src/main/java/com/abdulmo123/ecommerce/model/Order.java
+++ ecommerce-backend/src/main/java/com/abdulmo123/ecommerce/model/Order.java
@@ -1,6 +1,9 @@
 package com.abdulmo123.ecommerce.model;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Order {
@@ -10,5 +13,42 @@ public class Order {
     @Column(nullable = false, updatable = false)
     private Long id;
 
-    // TODO: add other fields
+    private Date createdDate;
+    private double totalPrice;
+
+    @OneToMany
+    @JoinColumn(name="order_id")
+    private List<Product> orderProducts = new ArrayList<>();
+
+    public Order () {}
+
+    public Order (Date createdDate, double totalPrice, List<Product> orderProducts) {
+        this.createdDate = createdDate;
+        this.totalPrice = totalPrice;
+        this.orderProducts = orderProducts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public List<Product> getOrderProducts() {
+        return orderProducts;
+    }
+
+    public void setOrderProducts(List<Product> orderProducts) {
+        this.orderProducts = orderProducts;
+    }
 }
